f400f6099507f509ad388faf367331e46033c491
*Lungile* <*lungile.simelane4@gmail.com*>
2024-04-27 23:21:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Keeping all text in JavaScript varaibles
+var titleText = "Make remote work";
+var paragraphText =
+  " Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.";
+var bootcamp = "Bootcamp 2024 by CodeTribe";
+var developer = "Developed by  Lungile Simelane";
+
+console.log("Title:" + titleText);
+console.log("Paragraph:" + paragraphText);
+console.log("Bootcamp:" + bootcamp);
+console.log("Developer:" + developer);
