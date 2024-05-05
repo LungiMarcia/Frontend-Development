e67affda76d23c665363d827d727988ca29b5ceb
*Lungile* <*lungile.simelane4@gmail.com*>
2024-05-05 02:18:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 var titleText = "Make remote work";
 var paragraphText =
   " Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.";
-var bootcamp = "Bootcamp 2024 by CodeTribe";
-var developer = "Developed by  Lungile Simelane";
+var footer =
+  " Bootcamp 2024 by CodeTribe. Developed by Lungile Simelane and hosted on Netlify.";
+var bootcampLink = "https://www.mlab.co.za/";
+var developerLink = "https://github.com/LungiMarcia";
+var hostLink = "https://frontend-web-page.netlify.app";
 
 console.log("Title:" + titleText);
 console.log("Paragraph:" + paragraphText);
-console.log("Bootcamp:" + bootcamp);
-console.log("Developer:" + developer);
+console.log("About the project:" + footer);
+console.log("Bootcamp:" + bootcampLink);
+console.log("Developer:" + developerLink);
+console.log("Host:" + hostLink);
